af37b7c72f6c6db83ccb712caafa480d25a7345b
brainzcode <98150590+brainzcode@users.noreply.github.com>
2022-11-18 20:12:05 +0100

    Final Commit

--- main.py
+++ main.py
@@ -1,11 +1,28 @@
 import datetime as dat
+import yagmail, random
+
+
+my_email = "testpythonmailer1@gmail.com"
+password = "foanjireptgrxvox"
 
 now = dat.datetime.now()
-year = now.year
-month = now.month
-day_of_week = now.weekday()
+weekday = now.weekday()
+if weekday == 3:
+    with open("quotes.txt") as quote_file:
+        all_quotes = quote_file.readlines()
+        quotes = random.choice(all_quotes)
 
-print(month)
+    print(quotes)
 
-date_of_birth = dat.datetime(year=1990, month=7, day=11)
-print(date_of_birth)
+    try:
+        # initializing the server connection
+        yag = yagmail.SMTP(user=my_email, password=password)
+        # sending the email
+        yag.send(to='testpythonmailer2@yahoo.com',
+                 subject='Monday Motivation',
+                 contents=f"<h1>{quotes}</h1>"
+                                                                                       'worked!</h1> '
+                                                                                       '<p>Just Running multiple tests</p>')
+        print("Email sent successfully")
+    except:
+        print("Error, email was not sent")
